676709deba7b2284fe67bc0109895a96639965e8
Nikhil Kumar Tiwari <88082428+nikhilkumar011@users.noreply.github.com>
2026-02-18 23:51:04 +0530

    Update Train.py

--- Model_train/Train.py
+++ Model_train/Train.py
@@ -4,19 +4,20 @@ def main():
     # Load the Nano model
     model = YOLO('yolov8n-seg.pt') 
 
-    # Start Low-Memory Training
+    # training starts here
     model.train(
         data='dataset.yaml', 
         epochs=1, 
-        imgsz=480,          # Reduced from 640 -> 480 (Massive memory saver)
-        batch=5,            # Reduced from 8 -> 4 (Fits in 6GB VRAM)
+        imgsz=480,          
+        batch=5,            
         device=0,
         project='Trials',
         name='v1_collab_run',
-        workers=1,          # Keep at 0 for stability
+        workers=1,         
         amp=True,
         exist_ok=True
     )
 
 if __name__ == '__main__':
+
     main()
